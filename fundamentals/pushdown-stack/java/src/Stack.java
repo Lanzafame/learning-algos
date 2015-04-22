596e61aa20ba5ff96f2b8d1365c9d9ceeb993021
Lanzafame <adrianlanzafame92@gmail.com>
2015-04-22 14:33:52 +1000

    cleaning up Idea config files

---
+++ fundamentals/pushdown-stack/java/src/Stack.java
@@ -0,0 +1,42 @@
+import java.util.Iterator;
+
+/**
+ * Created by lanzafame on 22/04/15.
+ */
+public class Stack<Item> implements Iterable<Item> {
+
+    /**
+     * Stack()
+     *
+     * Create an empty stack
+     */
+
+    /**
+     * void push(Item item)
+     *
+     * Add an item to the stack
+     */
+
+    /**
+     * Item pop()
+     *
+     * Remove the most recently added item
+     */
+
+    /**
+     * boolean isEmpty()
+     *
+     * Return whether the stack is empty
+     */
+
+    /**
+     * int size()
+     *
+     * Return the number items in the stack
+     */
+
+     @Override
+    public Iterator<Item> iterator() {
+        return null;
+    }
+}
