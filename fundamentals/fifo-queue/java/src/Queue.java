777901e3292912a0545490de83dadb378aac981b
Lanzafame <adrianlanzafame92@gmail.com>
2015-04-22 14:27:35 +1000

    Wrote Queue javadocs

---
+++ fundamentals/fifo-queue/java/src/Queue.java
@@ -0,0 +1,44 @@
+import java.util.Iterator;
+
+/**
+ * Created by lanzafame on 22/04/15.
+ */
+public class Queue<Item> implements Iterable<Item> {
+
+
+    /**
+     * Queue()
+     *
+     * Create an empty queue
+     */
+
+
+    /**
+     * void enqueue(Item item)
+     *
+     * Add an item to the queue
+     */
+
+    /**
+     * Item dequeue()
+     *
+     * Remove the least recently added item from the queue
+     */
+
+    /**
+     * boolean isEmpty()
+     *
+     * Return whether the queue is empty
+     */
+
+    /**
+     * int size()
+     *
+     * Return the number of items in the queue
+     */
+
+    @Override
+    public Iterator<Item> iterator() {
+        return null;
+    }
+}
